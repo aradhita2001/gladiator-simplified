cbc0e8c31e4e0d1d9daf88d9227de3dc1b8bfb6e
username <wecp-dummy@gmail.com>
2024-03-07 04:42:55 +0000

    wecp initial commit

---
+++ server/wecp-spring-boot-jwt-app/src/main/java/com/wecp/entities/UserTransaction.java
@@ -0,0 +1,108 @@
+package com.wecp.entities;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.resource.beans.internal.FallbackBeanInstanceProducer;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class UserTransaction {
+	
+	
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 Long id;
+	 
+	// @JsonIgnore
+	 @Transient
+	 String userId;
+	 
+	 Double balance;
+	 
+	 Double transactionAmount;
+	 
+	 String transactionType = "CREDIT"; //or "DEBIT"
+	 
+	 /**
+	  * @todo Assign a Many to One relationship with User entity with u_id as a join column name
+	  */
+	  @ManyToOne(fetch = FetchType.EAGER, optional = false)
+	  @JoinColumn(name = "u_id", nullable = false)
+	  @OnDelete(action = OnDeleteAction.CASCADE)
+	  @JsonIgnore
+	 User user;
+  
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public Double getTransactionAmount() {
+		return transactionAmount;
+	}
+
+	public void setTransactionAmount(Double transactionAmount) {
+		this.transactionAmount = transactionAmount;
+	}
+
+	public String getTransactionType() {
+		return transactionType;
+	}
+
+	public void setTransactionType(String transactionType) {
+		this.transactionType = transactionType;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	 
+	 
+	 
+	 
+
+}
